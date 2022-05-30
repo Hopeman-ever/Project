2d18961947f239f296490cfc430bdba09739e234
Hopeman <528419003@qq.com>
2022-05-30 17:51:42 +0800

    first commit

---
+++ Reggie_Project/CODE/reggie_take_out/src/main/java/com/hopeman/reggie/mapper/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.hopeman.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hopeman.reggie.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EmployeeMapper extends BaseMapper<Employee> {
+}
---
+++ Reggie_Project/CODE/reggie_take_out/src/main/java/com/hopeman/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.hopeman.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hopeman.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
